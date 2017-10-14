8833d142c5593c3e2eba20ba607554c6b7a42632
Sachin Saini <sachin17897@gmail.com>
2017-10-14 22:20:24 +0530

    routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+
+var Product = require('../models/products');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  Product.find((err, docs)=>{
+    var productChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize)
+    {
+      productChunks.push(docs.slice(i,i+chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });    
+  });
 });
+
 
 module.exports = router;
