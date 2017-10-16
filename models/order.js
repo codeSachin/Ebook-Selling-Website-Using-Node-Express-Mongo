43e7b69f77e91b34fc2c753a2165879be4937d69
Sachin Saini <sachin17897@gmail.com>
2017-10-16 21:16:29 +0530

    adding js

---
+++ models/order.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise
+
+var schema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    cart: {
+        type: Object,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    paymentId: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Order', schema);
