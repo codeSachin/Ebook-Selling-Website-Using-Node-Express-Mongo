1fb7e4dd73ab6a9530cb141da3d3b720d962aade
Sachin Saini <sachin17897@gmail.com>
2017-10-22 20:34:26 +0530

    Added Nodemailer Utility for sending emails on successful product purchase WOOHOOOO

---
+++ public/javascripts/nodemailer.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var nodemailer = require('nodemailer');
+
+var sendMail = (email)=>{
+
+
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        
+        auth: {
+            user: 'backupsachin1@gmail.com', // generated ethereal user
+            pass: '9041055490'  // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <backupsachin1@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Your Book will be deliverd within 7 business days!!!! Thank You for purchasing with us', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
+
+};
+
+module.exports.sendMail = sendMail;
